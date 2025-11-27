36f84ee4428870d686b89ffe190500e1c5d21c83
Aliefian <alief.nug21@gmail.com>
2025-11-27 15:49:47 +0700

    test

---
+++ main.js
@@ -0,0 +1,12 @@
+async function loadComponent(id, file) {
+  const element = document.getElementById(id);
+  const response = await fetch(file);
+  const html = await response.text();
+  element.innerHTML = html;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+});
+
+loadComponent("header", "/components/header.html");
+loadComponent("footer", "/components/footer.html");
