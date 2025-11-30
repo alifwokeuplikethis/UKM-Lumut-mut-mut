a2fb8914e7fe3052b30083ef325a25896b0f76ce
Ilham Mubarok <ilhmmbrok@gmail.com>
2025-11-30 20:42:16 +0700

    responsive

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 async function loadComponent(id, file) {
   const element = document.getElementById(id);
   const response = await fetch(file);
@@ -10,3 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 loadComponent("header", "/components/header.html");
 loadComponent("footer", "/components/footer.html");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await Promise.all([
+    loadComponent("header", "/components/header.html"),
+    loadComponent("footer", "/components/footer.html"),
+  ]);
+
+  const hamburger = document.getElementById("hamburger");
+  const navUl = document.querySelector("header nav ul");
+
+  if (hamburger && navUl) {
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navUl.classList.toggle("active");
+    });
+  }
+});
